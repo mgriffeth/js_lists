bae04ceaea315a45bf194c86810511789de79806
Matthew Griffeth <matthewgriffeth@Matthews-MacBook-Pro.local>
2014-10-01 16:59:47 -0400

    no5

--- main.js
+++ main.js
@@ -7,19 +7,24 @@ var prices = items.map(function(item){
 var total = prices.reduce(function(a, b){
   return a + b;
 });
-console.log(total)
+console.log(total);
 
 var averagePrice = parseInt(total / prices.length)
-console.log(averagePrice)
-
-
+console.log(averagePrice);
 
 //question 2
 var between = items.filter(function(item){
     return item.price > 13.99 && item.price < 18.01;
 });
-console.log(between)
-
-
+console.log(between);
 
 //question3
+var gbpCode = items.filter(function(x){
+  return x.currency_code === "GBP";
+});
+console.log(gbpCode);
+
+var titleAndPrice = gbpCode.map.call(gbpCode,function(x){
+  return (x.title + " $" + x.price);
+});
+console.log(titleAndPrice)
