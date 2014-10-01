261823873b0c12029e6dd0eb712ff0cd668e1162
Matthew Griffeth <matthewgriffeth@Matthews-MacBook-Pro.local>
2014-10-01 16:15:46 -0400

    no3

--- main.js
+++ main.js
@@ -11,4 +11,15 @@ console.log(total)
 
 var averagePrice = parseInt(total / prices.length)
 console.log(averagePrice)
+
+
+
 //question 2
+var between = items.filter(function(item){
+    return item.price > 13.99 && item.price < 18.01;
+});
+console.log(between)
+
+
+
+//question3
